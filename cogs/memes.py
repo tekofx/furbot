f2d42ebce37392514adab0130088f95913c40b75
Teko <tekofxx@gmail.com>
2021-04-28 14:37:15 +0200

    Arreglado comando suicidio para que no acepte argumento

--- cogs/memes.py
+++ cogs/memes.py
@@ -152,23 +152,6 @@ class memes(commands.Cog):
 
 
     @commands.command()
-    async def españa(self, context, *, user: discord.Member = None):
-        """Arriba España!!!
-            
-            Uso: fur españa "@<usuario>
-        """
-        await context.channel.send('Procesando video')
-        create_video_meme("españa", get_user(context, user))
-
-        # Send meme
-        await context.channel.send(file=discord.File(memeTemplatesPath + "output.mp4"))
-        log("info","Meme sent")
-
-        # Delete output
-        delete_files(['output.mp4'])
-
-
-    @commands.command()
     async def burn(self, context, *, user: discord.Member = None):
         """Quema a tus amigos :)
             
@@ -204,54 +187,6 @@ class memes(commands.Cog):
 
         # Delete user avatar and output
         delete_files(('01.webp', 'output.png', '01.png'))
-
-
-    @commands.command()
-    async def betis(self, context, *, user: discord.Member = None):
-        """Olé mi Betis
-
-        """
-        await context.channel.send('Procesando video')
-        log("info","Meme sent")
-        create_video_meme("betis", get_user(context, user))
-
-        # Send meme
-        await context.channel.send(file=discord.File(memeTemplatesPath + "output.mp4"))
-        log("info","Meme sent")
-
-        # Delete output
-        delete_files(['output.mp4'])
-
-
-    @commands.command()
-    async def communist(self, context, *, user: discord.Member = None):
-        """Viva el proletariado"""
-        await context.channel.send('Procesando video')
-        create_video_meme("communist", get_user(context, user))
-
-        # Send meme
-        await context.channel.send(file=discord.File(memeTemplatesPath + "output.mp4"))
-        log("info","Meme sent")
-
-        # Delete output
-        delete_files(['output.mp4'])
-
-
-    @commands.command()
-    async def falange(self, context, *, user: discord.Member = None):
-        """Viva Francisco Franco!!!
-            
-            Uso: fur falange "@<usuario>
-        """
-        await context.channel.send('Procesando video')
-        create_video_meme("falange", get_user(context, user))
-
-        # Send meme
-        await context.channel.send(file=discord.File(memeTemplatesPath + "output.mp4"))
-        log("info","Meme sent")
-
-        # Delete output
-        delete_files(['output.mp4'])
 
 
     @commands.command()
@@ -530,45 +465,13 @@ class memes(commands.Cog):
         log("info","Meme sent")
 
 
-    @commands.command()
-    async def aragon(self, context, *, user: discord.Member = None):
-        """Viva Aragón! (pero menos que León)"""
-
-        await context.channel.send('Procesando video')
-        create_video_meme("aragon", get_user(context, user))
-
-        # Send meme
-        await context.channel.send(file=discord.File(memeTemplatesPath + "output.mp4"))
-        log("info","Meme sent")
-
-
-        # Delete output
-        delete_files(['output.mp4'])
-
-
-    @commands.command()
-    async def leon(self, context, *, user: discord.Member = None):
-        """Viva León!"""
-
-        await context.channel.send('Procesando video')
-        log("info","Meme sent")
-        create_video_meme("leon", get_user(context, user))
-
-        # Send meme
-        await context.channel.send(file=discord.File(memeTemplatesPath + "output.mp4"))
-        log("info","Meme sent")
-
-
-        # Delete output
-        delete_files(['output.mp4'])
-
-
+   
     @commands.command()
     async def suicidio(self, context, *, user: discord.Member = None):
         """Es hora del suisidio"""
 
         # Get user avatar
-        avatarUrl = get_user(context, user).avatar_url
+        avatarUrl = context.author.avatar_url
 
         create_meme(('suicidio', '01'), avatarUrl, 53, (0, 0, 172, 181), True)
 
