7d6348623f73eec9ff663a691207cf3a495b9228
Teko <tekofxx@gmail.com>
2021-05-11 12:35:09 +0200

    Añadido fur carne

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -6,7 +6,10 @@ import random
 from cogs.functions import *
 import wget
 import os
-from cogs.functions import memeTemplatesPath
+from cogs.functions import * 
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
 
 
 
@@ -66,6 +69,50 @@ class utilities(commands.Cog):
         embed.add_field(name="Roles", value=roles, inline=False)
         await context.channel.send(embed=embed)
 
+
+    @commands.command()
+    async def carne(self,context, *,user : discord.Member=None):
+        """Muestra tu carnet como miembro de Villa Furrense
+        """
+
+        # Get user info
+        usr=get_user(context, user)
+        name=usr.display_name
+        rango=str(get_user_ranks(usr))
+        roles=get_user_roles(usr)
+        fecha=str(usr.joined_at)
+        fecha=fecha.split()
+        print('0')
+        var = "wget -O %s%s %s" % (memeTemplatesPath, '01'+".webp", usr.avatar_url)
+        os.system(var)
+        # get_user_avatar(usr,'01')
+        convert_pic(memeTemplatesPath+'01.webp','01',300)
+        print('1')
+        print('2')
+        user_avatar=memeTemplatesPath+'01.png'
+        avatar=Image.open(user_avatar)
+        # Open carnet to draw
+        output = Image.open("carnet.png").convert("RGBA")
+        draw = ImageDraw.Draw(output)
+        font = ImageFont.truetype(memeTemplatesPath + "Calibri.ttf", 50)
+
+        # Draw name
+        draw.text(((415,303)),name,font=font,fill=(0,0,0,255))
+
+        # Draw rank
+        draw.text(((415,430)),rango,font=font,fill=(0,0,0,255))
+
+        # Draw time in server
+        draw.text(((415,563)),fecha[0],font=font,fill=(0,0,0,255))
+
+
+        print('3')
+        # Add avatar
+        output.paste(avatar, (50,300))
+
+        output.save(memeTemplatesPath+"output.png","PNG")
+        await context.channel.send(file=discord.File(memeTemplatesPath+'output.png'))
+        delete_files(('01.webp','output.png','01.png'))
     @commands.command()
     async def avatar(self,context, *,user : discord.Member=None):
         """Obtén la imagen de perfil de alguien"""
