81b67bc551a18ca9a313540673ccb00279ca22bf
Teko <tekofxx@gmail.com>
2021-05-21 19:45:56 +0200

    Arreglos en administracion

--- cogs/administration.py
+++ cogs/administration.py
@@ -20,19 +20,19 @@ class Administration(commands.Cog):
     
     @commands.command(name='activity')
     @commands.check(is_owner)
-    async def change_activity(self, context, activity_test:str, activity:str):
+    async def change_activity(self, context, activity_name:str, activity:str):
         """ Cambiar actividad del bot 
         """
         if activity.lower()=='watching':
-            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity_test))
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity_name))
         elif activity.lower()=='streaming':
-            await self.bot.change_presence(activity=discord.Streaming(name="My Stream", url=activity_test))
+            await self.bot.change_presence(activity=discord.Streaming(name="My Stream", url=activity_name))
         elif activity.lower()=='listening':
-            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity_test))
+            await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity_name))
         else:
-            await self.bot.change_presence(activity=discord.Game(name=activity_test))
+            await self.bot.change_presence(activity=discord.Game(name=activity_name))
 
-        await context.channel.send("Cambiada actividad a "+activity_test)
+        await context.channel.send("Cambiada actividad a "+activity_name)
  
 def setup(bot):
     bot.add_cog(Administration(bot))
