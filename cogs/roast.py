e28dc8747dc3955c4dc7dac82d6f63636d5e9e51
Teko <tekofxx@gmail.com>
2021-06-28 19:34:02 +0100

    Añadida respuesta en fur teodio

--- cogs/roast.py
+++ cogs/roast.py
@@ -5,59 +5,17 @@ from discord.ext import commands
 import logging
 from cogs.functions import insults_txt, is_admin, magnet_id, animos_txt
 
-
-
 class roast(commands.Cog):
     """Insultos varios"""
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def aputo(self, context):
-        """Asriel puto"""
-        await context.channel.send("Asriel puto")
-
-    @commands.command()
-    async def cputo(self, context):
-        """Cracker puto"""
-        await context.channel.send("Cracker puto")
-
-    @commands.command()
-    async def caputo(self, context):
-        """Capi puto"""
-        await context.channel.send("Capi puto")
-
-    @commands.command()
-    async def daputo(self, context):
-        """Dachi puto"""
-        await context.channel.send("Dachi puto")
-
-    @commands.command()
-    async def gaputo(self, context):
-        """Gala puto"""
-        await context.channel.send("Gala puto")
-
+    
     @commands.command()
     async def garich(self, context):
         """Gala rich"""
         output = "Gala rich porque compra Axe, tiene 5 Alexas, tiene una limpiadora que le regala cosas y tiene una tele 4k"
         await context.channel.send(output)
-
-    @commands.command()
-    async def magnetputo(self, context):
-        """Magnet puto"""
-        await context.channel.send("Magnet puto")
-
-    @commands.command()
-    async def peputo(self, context):
-        """Pen puto"""
-        await context.channel.send("Pen puto")
-
-    @commands.command()
-    async def teputo(self, context):
-        """Teko puto"""
-        await context.channel.send("Teko puto")
 
     @commands.command()
     async def tecute(self, context):
@@ -66,6 +24,7 @@ class roast(commands.Cog):
             await context.channel.send("Teko cute")
         else:
             await context.channel.send("Teko NO cute")
+
     @commands.command()
     async def teodio(self, context, *, user: discord.Member = None):
         """Muestra tu odio al bot"""
@@ -74,34 +33,7 @@ class roast(commands.Cog):
         else:
             usuario = user.mention
         output = "{} %s " % ("Yo te odio mas pedazo de basura")
-        await context.channel.send(output.format(usuario))
-
-    @commands.command()
-    async def sliputa(self, context):
-        """Sliva puto"""
-        await context.channel.send("Sliva puta")
-
-    @commands.command()
-    async def steputo(self, context):
-        """Stewan puto"""
-        await context.channel.send("Stewan puto")
-
-    @commands.command(name="tputo")
-    async def tputo(self, context):
-        """Thedax puto"""
-        await context.channel.send("Thedax puto")
-
-    @commands.command()
-    async def zaputo(self, context):
-        """Zaffy puto"""
-        await context.channel.send("Zaffy puto")
-
-    @commands.command()
-    async def crackergei(self, context):
-        """cracker gei"""
-        await context.channel.send(
-            file=discord.File("/home/teko/bots/furbot/stickers/cracker.png")
-        )
+        await context.reply(output.format(usuario))
 
     @commands.command()
     async def insult(self, context, *, user: discord.Member = None):
@@ -134,8 +66,6 @@ class roast(commands.Cog):
             await context.channel.send("Insulto/s añadido/s")
         except:
             logging.error("Error at getting insults.txt")
-
-
 
     @commands.command()
     async def animo(self, context, *, user: discord.Member = None):
