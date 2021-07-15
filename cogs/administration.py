e72394612ebde84ef6fb5e0c5761f9cb0f59f06d
Teko <tekofxx@gmail.com>
2021-07-15 20:07:32 +0100

    Eliminado paquete os no necesario

--- cogs/administration.py
+++ cogs/administration.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import os
 from cogs.functions import delete_content_in_file, is_admin, help_txt, admin, activity_txt, write_in_file
 import logging
 
