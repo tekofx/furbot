9f6581bbd90b94f5bb566fa99927fe4e17838b8e
Teko <tekofxx@gmail.com>
2021-09-30 09:31:19 +0100

    Añadido script para ejecutar en docker

---
+++ run_in_docker.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+
+
+# Get ports available
+print("Getting port available")
+result = subprocess.Popen(
+    "docker ps --format '{{.Ports}}' | cut -d ' ' -f2 | cut -b 4-5 ",
+    shell=True,
+    stdout=subprocess.PIPE,
+)
+
+result = result.stdout.read()
+result = result.decode("utf-8")
+result = result.split("\n")
+
+ports = []
+for x in result[:-1]:
+    ports.append(int(x))
+
+count = 80
+while True:
+    if count not in ports:
+        port = count
+        break
+    count += 1
+print("Port available: ", port)
+
+
+# Get path
+path = os.path.dirname(os.path.abspath(__file__))
+
+# Build
+print("Building furbot docker image")
+os.system("docker build --no-cache -t uwubot .")
+
+
+# Run
+print("Running docker container")
+docker_run = """ docker run -d \
+    --name uwu \
+    --mount type=bind,src={path}/files/,dst=/bot/files/  \
+    --mount type=bind,src={path}/src/,dst=/bot/src \
+    --restart=unless-stopped \
+    -p {port}:80 uwu """
+docker_run = docker_run.format(path=path, port=port)
+
+
+os.system(docker_run)
