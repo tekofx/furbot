0ae76f96baeda7752842dc71c2bf041eb64f6eaa
Teko <tekofxx@gmail.com>
2021-10-13 19:21:57 +0200

    Modificado mensaje de error cuando no hay suficientes parametros

--- src/plugins/listeners.py
+++ src/plugins/listeners.py
@@ -1,5 +1,6 @@
 import hikari
 import lightbulb
+from lightbulb.help import HelpCommand
 
 
 class Listeners(lightbulb.Plugin):
@@ -10,6 +11,7 @@ class Listeners(lightbulb.Plugin):
     @lightbulb.listener(lightbulb.events.CommandErrorEvent)
     async def on_command_error(self, event: lightbulb.events.CommandErrorEvent):
         error = event.exception
+        command = event.command
         if isinstance(error, lightbulb.errors.CommandIsOnCooldown):
             return await event.message.reply("Command is on cooldown")
 
@@ -31,7 +33,13 @@ class Listeners(lightbulb.Plugin):
             )
 
         if isinstance(error, lightbulb.errors.NotEnoughArguments):
-            return await event.message.respond("Error: Faltan argumentos")
+            await event.message.respond(
+                "Error: Faltan argumentos, comprueba como se usa el comando {}".format(
+                    command.name
+                )
+            )
+            await lightbulb.HelpCommand.send_command_help(self, event.context, command)
+            return
 
 
 def load(bot: lightbulb.Bot):
