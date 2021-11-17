256a24ea2558ba95f336dabb9c9c8707d5180f6e
Teko <tekofxx@gmail.com>
2021-11-17 22:49:31 +0100

    Eliminado logger de bot

--- src/utils/bot.py
+++ src/utils/bot.py
@@ -205,26 +205,3 @@ class Bot(lightbulb.Bot):
         self.event_manager.subscribe(hikari.MemberDeleteEvent, self.on_leave_member)
 
         super().run()
-
-
-def build_logger(name=__name__, level=logging.DEBUG):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(level)
-
-    file_handler = logging.FileHandler("bot.log", mode="a")
-    file_handler.setLevel(level)
-
-    formatter = logging.Formatter(
-        fmt="{levelname} - {asctime} in {name}:\n{message}",
-        datefmt="%b %d %H:%M:%S",
-        style="{",
-    )
-
-    file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-    return logger
