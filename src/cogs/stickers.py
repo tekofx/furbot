ca0b8814a44c1829ecc0037ebf9356baf8ce26cc
Teko <tekofxx@gmail.com>
2021-12-21 12:22:04 +0100

    Añadido cog stickers

---
+++ src/cogs/stickers.py
@@ -0,0 +1,100 @@
+import logging
+from pathlib import Path
+import nextcord
+from nextcord.ext import commands
+import requests
+from utils.functions import stickers_path, convert_pic
+import os
+
+log = logging.getLogger(__name__)
+
+
+class stickers(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command(name="addsticker")
+    async def add_sticker(self, ctx: commands.Context, sticker_name: str):
+        """Añade un sticker
+
+        Uso: seleccionar una imagen y en el cuadro de "añadir comentario"
+        poner fur add <nombre_sticker>
+        """
+
+        # If not image provided
+        if not ctx.message.attachments:
+            await ctx.send("Error: No se ha añadido una imagen")
+            return
+
+        # Checks if a picture is correct
+        sticker_extension = ctx.message.attachments[0].url.split(".")[-1]
+        if check_sticker(sticker_name, sticker_extension) == 0:
+            await ctx.send(
+                "Error: Ya existe un sticker con el nombre {}".format(sticker_name)
+            )
+            return
+
+        if sticker_extension == "jpg":
+            sticker_fileName = sticker_name + ".jpg"
+        else:
+            sticker_fileName = sticker_name + ".png"
+
+        stickerUrl = ctx.message.attachments[0].url
+
+        r = requests.get(stickerUrl, allow_redirects=True)
+        open(stickers_path + sticker_fileName, "wb").write(r.content)
+
+        convert_pic(stickers_path + sticker_fileName, sticker_name)
+
+        if sticker_extension == "jpg":
+            os.remove(stickers_path + sticker_fileName)
+        await ctx.send("Sticker " + sticker_name + " añadido")
+
+    @commands.command(name="list")
+    async def list_stickers(self, ctx: commands.Context):
+        """Lista de los stickers añadidos"""
+        output = os.listdir(stickers_path)
+        output.sort()
+        output[:] = [s.replace(".png", "") for s in output]
+        output = ", ".join(output)
+        await ctx.send(output)
+
+    @commands.command("s")
+    async def use_sticker(self, ctx: commands.Context, sticker):
+        """Usar un sticker
+
+        Uso: fur s <nombre_sticker>
+        """
+        if Path(stickers_path + sticker + ".png").is_file():
+            stickerName = stickers_path
+            stickerName += sticker
+            stickerName += ".png"
+            await ctx.send(file=nextcord.File(stickerName))
+            log.info("Sticker " + sticker + " sent")
+        else:
+
+            await ctx.send("No existe el sticker " + sticker)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(stickers(bot))
+
+
+def check_sticker(stickerName: str, stickerExtension: str):
+    """checks if a sticker already exists and if file extension is correct
+
+    Args:
+        stickerName ([String]): [Name of sticker to add]
+        stickerExtension ([String]): [Extension of sticker to add]
+
+    Returns:
+        [0]: [name used by other sticker]
+        [1]: [if file extension is correct, must be jpg or png]
+    """
+    string = os.listdir(stickers_path)
+    string[:] = [s.replace(".png", "") for s in string]
+    string[:] = [s.replace("'", "") for s in string]
+    if stickerName in string:
+        return 0
+    if stickerExtension in ["jpg", "png"]:
+        return 1
