8e70fc59c3978cab24f8a50924ab46adbeb0ea6f
Teko <tekofxx@gmail.com>
2021-12-21 22:28:30 +0100

    Eliminado fetch de server

--- src/utils/bot.py
+++ src/utils/bot.py
@@ -37,7 +37,6 @@ class Bot(commands.Bot):
         log.info("Loaded memes channel")
         self.audit_channel = await self.fetch_channel(os.getenv("AUDIT_CHANNEL"))
         log.info("Loaded audit channel\n")
-        self.server = await self.fetch_guild(os.getenv("VILLAFURRENSE"))
 
         # Load cogs
         cogs = os.listdir("src/cogs/")
