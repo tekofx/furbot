aa5032f8cb39e104fde03fceff3941e64d8a783a
tekofx <tekofxx@gmail.com>
2022-06-15 14:28:37 +0200

    cambio

--- src/cogs/tasks.py
+++ src/cogs/tasks.py
@@ -211,7 +211,7 @@ class tasks(commands.Cog):
 
     @free_games.before_loop
     @discord_status.before_loop
-    # @meme.before_loop
+    @meme.before_loop
     @joined_date.before_loop
     async def prep(self):
         """Waits some time to execute tasks"""
