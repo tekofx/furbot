8930ba41d122fc9d521d19e519c70c54146b8ffa
tekofx <tekofxx@gmail.com>
2022-06-27 11:42:45 +0200

    Arreglado tipos de record en eliminar records

--- src/cogs/tasks.py
+++ src/cogs/tasks.py
@@ -65,7 +65,7 @@ class tasks(commands.Cog):
     async def remove_records_from_previous_day(self):
         """Removes records from 2 days ago"""
         for guild in self.bot.guilds:
-            remove_records_from_a_date(guild, ("test1", "test2"))
+            remove_records_from_a_date(guild, ("meme", "incident"))
 
     @tasks.loop(hours=6)
     async def update_users(self):
