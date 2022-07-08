dc750da0f856646ac454e9d6ec2b38704664ec15
tekofx <tekofxx@gmail.com>
2022-07-08 16:35:31 +0200

    Modificados colores de niveles

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -14,32 +14,64 @@ def getLogger(name):
 class CustomFormatter(logging.Formatter):
     """Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629"""
 
-    grey = "\x1b[38;21m"
-    green = "\033[32m"
-    blue = "\x1b[38;5;39m"
-    yellow = "\x1b[38;5;226m"
-    red = "\x1b[38;5;196m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    LIGHT_BLUE = "\033[1;34m"
+    LIGHT_PURPLE = "\033[1;35m"
+    LIGHT_CYAN = "\033[1;36m"
+    LIGHT_WHITE = "\033[1;37m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"
+    RESET = "\x1b[0m"
 
     def __init__(self):
         super().__init__()
-        self.fmt = f"{self.green}%(asctime)s{self.reset} {self.blue}%(levelname)s{self.reset} {self.red}%(name)s{self.reset} %(message)s"
-        self.fmt_guild = f"{self.green}%(asctime)s{self.reset} {self.blue}%(levelname)s{self.reset} {self.red}%(name)s{self.reset} {self.yellow}%(guild)s{self.reset} %(message)s"
+        self.fmt = (
+            f"{self.GREEN}%(asctime)s{self.RESET} {self.PURPLE}%(name)s{self.RESET} "
+        )
+        self.fmt_guild = (
+            f"{self.GREEN}%(asctime)s{self.RESET} {self.PURPLE}%(name)s{self.RESET} "
+        )
 
         self.FORMATS = {
-            logging.DEBUG: self.fmt + self.reset,
-            logging.INFO: self.fmt + self.reset,
-            logging.WARNING: self.fmt + self.reset,
-            logging.ERROR: self.fmt + self.reset,
-            logging.CRITICAL: self.fmt + self.reset,
+            logging.DEBUG: self.fmt
+            + f"{self.BLUE}%(levelname)s{self.RESET} %(message)s",
+            logging.INFO: self.fmt
+            + f"{self.BLUE}%(levelname)s{self.RESET} %(message)s",
+            logging.WARNING: self.fmt
+            + f"{self.YELLOW }%(levelname)s{self.RESET} %(message)s",
+            logging.ERROR: self.fmt
+            + f"{self.LIGHT_RED}%(levelname)s{self.RESET} %(message)s",
+            logging.CRITICAL: self.fmt
+            + f"{self.RED}%(levelname)s{self.RESET} %(message)s",
         }
         self.FORMATS_GUILD = {
-            logging.DEBUG: self.fmt_guild + self.reset,
-            logging.INFO: self.fmt_guild + self.reset,
-            logging.WARNING: self.fmt_guild + self.reset,
-            logging.ERROR: self.fmt_guild + self.reset,
-            logging.CRITICAL: self.fmt_guild + self.reset,
+            logging.DEBUG: self.fmt_guild
+            + f"{self.BLUE}%(levelname)s{self.RESET} {self.YELLOW}%(guild)s{self.RESET} %(message)s",
+            logging.INFO: self.fmt_guild
+            + f"{self.BLUE}%(levelname)s{self.RESET} {self.YELLOW}%(guild)s{self.RESET} %(message)s",
+            logging.WARNING: self.fmt_guild
+            + f"{self.YELLOW}%(levelname)s{self.RESET} {self.YELLOW}%(guild)s{self.RESET} %(message)s",
+            logging.ERROR: self.fmt_guild
+            + f"{self.LIGHT_RED}%(levelname)s{self.RESET} {self.YELLOW}%(guild)s{self.RESET} %(message)s",
+            logging.CRITICAL: self.fmt_guild
+            + f"{self.RED}%(levelname)s{self.RESET} {self.YELLOW}%(guild)s{self.RESET} %(message)s",
         }
 
     def format(self, record):
