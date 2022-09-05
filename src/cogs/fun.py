23e695a33a7e3a1f4b9927aaa0e2876dd8e28401
tekofx <tekofxx@gmail.com>
2022-09-05 16:24:56 +0200

    Slash commands en fun

--- src/cogs/fun.py
+++ src/cogs/fun.py
@@ -2,78 +2,105 @@ import nextcord
 from nextcord.ext import commands
 import random
 from utils.bot import Bot
+from nextcord import Interaction
+
+test_guild = 0
 
 
 class fun(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
-    @commands.command()
-    async def penis(self, ctx: commands.Context, user: nextcord.Member = None):
-        """Buena tula"""
-
+    @nextcord.slash_command(
+        guild_ids=[test_guild],
+        name="penis",
+        description="Buena tula",
+    )
+    async def penis(self, interaction: Interaction, usuario: nextcord.Member = None):
         num = random.randint(1, 15)
         output = "El pene de {}\n"
-        if user is None:
-            usuario = ctx.author.display_name
+        if usuario is None:
+            usuario = interaction.user.mention
         else:
-            usuario = user.display_name
+            usuario = usuario.mention
 
         output += "8"
         for x in range(num):
             output += "="
         output += "D"
-        await ctx.send(output.format(usuario))
+        await interaction.send(output.format(usuario))
 
-    @commands.command()
-    async def communist(self, ctx: commands.Context, user: nextcord.Member = None):
-        """Serás un comunista bolivariano que apoya al Coletas?"""
-
+    @nextcord.slash_command(
+        guild_ids=[test_guild],
+        name="communist",
+        description="Serás un comunista bolivariano que apoya al Coletas?",
+    )
+    async def communist(
+        self, interaction: Interaction, usuario: nextcord.Member = None
+    ):
         num = random.randint(0, 100)
+
         output = "{} es {}% comunista"
-        if user is None:
-            usuario = ctx.author.display_name
+
+        if usuario is None:
+            usuario = interaction.user.mention
         else:
-            usuario = user.display_name
-        await ctx.send(output.format(usuario, num))
+            usuario = usuario.mention
 
-    @commands.command()
-    async def capitalist(self, ctx: commands.Context, user: nextcord.Member = None):
-        """Serás capitalista y te convertirás en el nuevo lobo de Wall Street?"""
+        await interaction.send(output.format(usuario, num))
 
+    @nextcord.slash_command(
+        guild_ids=[test_guild],
+        name="capitalist",
+        description="Serás capitalista y te convertirás en el nuevo lobo de Wall Street?",
+    )
+    async def capitalist(
+        self, interaction: Interaction, usuario: nextcord.Member = None
+    ):
         num = random.randint(0, 100)
+
         output = "{} es {}% capitalista"
-        if user is None:
-            usuario = ctx.author.display_name
+
+        if usuario is None:
+            usuario = interaction.user.mention
         else:
-            usuario = user.display_name
-        await ctx.send(output.format(usuario, num))
+            usuario = usuario.mention
 
-    @commands.command()
-    async def gay(self, ctx: commands.Context, user: nextcord.Member = None):
-        """Maricon el ultimo"""
+        await interaction.send(output.format(usuario, num))
 
+    @nextcord.slash_command(
+        guild_ids=[test_guild],
+        name="gay",
+        description="Maricon el ultimo",
+    )
+    async def gay(self, interaction: Interaction, usuario: nextcord.Member = None):
         num = random.randint(0, 100)
+
         output = "{} es {}% gay"
-        if user is None:
-            usuario = ctx.author.display_name
+
+        if usuario is None:
+            usuario = interaction.user.mention
         else:
-            usuario = user.display_name
+            usuario = usuario.mention
 
-        await ctx.send(output.format(usuario, num))
+        await interaction.send(output.format(usuario, num))
 
-    @commands.command(name="str8")
-    async def straight(self, ctx: commands.Context, user: nextcord.Member = None):
-        """Super str8 el ultimo"""
-
+    @nextcord.slash_command(
+        guild_ids=[test_guild],
+        name="str8",
+        description="Super str8 el ultimo",
+    )
+    async def straight(self, interaction: Interaction, usuario: nextcord.Member = None):
         num = random.randint(0, 100)
-        output = "{} es {}% hetero"
-        if user is None:
-            usuario = ctx.author.display_name
-        else:
-            usuario = user.display_name
 
-        await ctx.send(output.format(usuario, num))
+        output = "{} es {}% gay"
+
+        if usuario is None:
+            usuario = interaction.user.mention
+        else:
+            usuario = usuario.mention
+
+        await interaction.send(output.format(usuario, num))
 
 
 def setup(bot: commands.Bot):
