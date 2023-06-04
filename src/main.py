f08ebd47945ac30a8d5325509e10394937e622a2
tekofx <tekofxx@gmail.com>
2023-06-04 17:55:38 +0200

    Eliminada gestion de envs

--- src/main.py
+++ src/main.py
@@ -16,43 +16,13 @@ os.chdir(working_dir)
 # Set process name
 setproctitle.setproctitle("furbot")
 
-# Env variables
-env_vars = [
-    "DISCORD_TOKEN",
-    "VILLAFURRENSE",
-    "REDDIT_CLIENT_ID",
-    "REDDIT_CLIENT_SECRET",
-    "REDDIT_USER_AGENT",
-    "TWITTER_CONSUMER_KEY",
-    "TWITTER_CONSUMER_SECRET",
-    "TWITTER_ACCESS_TOKEN",
-    "TWITTER_ACCESS_TOKEN_SECRET",
-]
-
-
-# Remove previous env variables from environment
-# and check if any missing env variable
-for var in env_vars:
-    if var in os.environ:
-        del os.environ[var]
-    else:
-        try:
-            os.getenv(var)
-        except Exception as error:
-            log.error(
-                "Error: Missing environmental variable {} in .env file".format(var)
-            )
-            sys.exit()
-
 
 if "-t" in sys.argv:
     log.info("Executing in TEST mode")
-    load_dotenv("env/tests.env")
+    load_dotenv("dev.env")
 else:
     log.info("Executing in NORMAL mode")
 
-    load_dotenv("env/furbot.env")
 
-token = os.getenv("DISCORD_TOKEN")
-bot = Bot(token)
+bot = Bot(os.environ["DISCORD_TOKEN"])
 bot.run()
