850c501acdb5fc6e09b8c945ded9ab1458fc36d2
tekofx <tekofxx@gmail.com>
2023-06-06 12:30:42 +0200

    Added basic table creation and inserts

--- src/core/database.py
+++ src/core/database.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from core import logger
+import logger
 import os
 import mysql.connector
 
@@ -20,9 +20,9 @@ drop_tables = """DROP TABLE IF EXISTS users, roles, sentences, records, channels
 
 roles_table = """ CREATE TABLE IF NOT EXISTS roles (
                                     id int(18),
-                                    guild int(18) integer NOT NULL,
-                                    name varchar(20) NOT NULL ,
-                                    type text varchar(20)
+                                    guild int(18) NOT NULL,
+                                    name varchar(20) NOT NULL,
+                                    type varchar(20),
                                     
                                     CONSTRAINT PK_roles PRIMARY KEY (id, guild)
                                 );"""
@@ -32,30 +32,42 @@ role_insert="""INSERT INTO roles (id, guild, name, type) VALUES (%s, %s, %s, %s)
 
 
 records_table = """ CREATE TABLE IF NOT EXISTS records (
-                                    id integer PRIMARY KEY AUTOINCREMENT,
-                                    guild_id integer NOT NULL,
-                                    type text NOT NULL ,
-                                    account text,
-                                    record text NOT NULL,
-                                    date date NOT NULL
+                                    id int(18) AUTO_INCREMENT,
+                                    guild int(18) ,
+                                    type varchar(20) NOT NULL,
+                                    account varchar(20) NOT NULL,
+                                    record varchar(20) NOT NULL,
+                                    date datetime NOT NULL,
+                                    
+                                    CONSTRAINT PK_records PRIMARY KEY (id, guild)
                                 ); """
+                                
+record_insert="""INSERT INTO records (id, guild, type, account, record, date) VALUES (%s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE record=%s, date=%s;"""
 
 channels_table = """ CREATE TABLE IF NOT EXISTS channels (
-                                    id integer NOT NULL PRIMARY KEY,
-                                    guild_id integer NOT NULL,
-                                    type text,
-                                    policy text NOT NULL,
-                                    name text NOT NULL
+                                    id int(18),
+                                    guild int(18),
+                                    type varchar(20) NOT NULL,
+                                    policy varchar(20) NOT NULL,
+                                    name varchar(20) NOT NULL,
+                                    
+                                    CONSTRAINT PK_channels PRIMARY KEY (id, guild)
                                 ); """
+                                
+channel_insert="""INSERT INTO channels (id, guild, type, policy, name) VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE policy=%s, name=%s;"""
 posts_table = """ CREATE TABLE IF NOT EXISTS posts (
-                                    id integer PRIMARY KEY AUTOINCREMENT,
-                                    guild_id integer NOT NULL,
-                                    channel_id integer NOT NULL,
-                                    visibility text NOT NULL, 
-                                    service text NOT NULL,
-                                    account text NOT NULL, 
-                                    interval integer NOT NULL
+                                    id int(18) AUTO_INCREMENT,
+                                    guild int(18) NOT NULL,
+                                    channel int(18) NOT NULL,
+                                    visibility ENUM('sfw','nsfw') NOT NULL, 
+                                    service varchar(20) NOT NULL,
+                                    account varchar(40) NOT NULL,
+                                    frequency int(6) NOT NULL,
+                                    
+                                    CONSTRAINT PK_posts PRIMARY KEY (id, guild)
                                 ); """
+                                
+post_insert="""INSERT INTO posts (id, guild, channel, visibility, service, account, frequency) VALUES (%s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE visibility=%s, service=%s, account=%s, frequency=%s;"""
                                 
 tables = [
     users_table,
@@ -97,8 +109,20 @@ class Database:
         self.connection.close()
 
 
-load_dotenv("../dev.env")
+load_dotenv("../../dev.env")
 db=Database()
 db.execute_query(drop_tables)
+
+# Create tables
 db.execute_query(users_table)
+db.execute_query(roles_table)
+db.execute_query(records_table)
+db.execute_query(channels_table)
+db.execute_query(posts_table)
+
+# Insert data
 db.execute_query(user_insert,(1,1,"test","2020-01-01","2020-01-01","test","2020-01-01","2020-01-01"))
+db.execute_query(role_insert,(1,1,"test","test","test","test"))
+db.execute_query(record_insert,(1,1,"test","test","test","2020-01-01","test","2020-01-01"))
+db.execute_query(post_insert,(1,1,1,"sfw","test","test",1,"sfw","test","test",1))
+db.execute_query(channel_insert,(1,1,"test","test","test","test","test"))
