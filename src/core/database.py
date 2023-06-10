85d22a7951b89bfd665a0d7751394d54dddcd909
tekofx <tekofxx@gmail.com>
2023-06-10 20:10:41 +0200

    removed print

--- src/core/database.py
+++ src/core/database.py
@@ -101,7 +101,6 @@ log = logger.getLogger(__name__)
 
 class Database:
     def __init__(self):
-        print(os.getenv("MYSQL_USER"),os.getenv("MYSQL_PASSWORD"),os.getenv("MYSQL_DATABASE"))
         self.connection = mysql.connector.connect(
             host="localhost",
             user=os.getenv("MYSQL_USER"),
