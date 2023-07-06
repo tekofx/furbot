8df3690b5482b2ccd127bb05db1d672a0ba1d609
tekofx <tekofxx@gmail.com>
2023-07-06 13:43:52 +0200

    Added title and description to votacion

--- src/cogs/utils.py
+++ src/cogs/utils.py
@@ -87,15 +87,15 @@ class Utils(commands.Cog):
     async def votacion(
         self,
         interaction: Interaction,
-        opciones:int
+        num_opciones:int
     ):
         """Crea una votacion
         
         Args:
-            opciones: Número de opciones entre las que votar
+            num_opciones: Número de opciones entre las que votar
 
         """
-        await interaction.response.send_modal(Modal(opciones))
+        await interaction.response.send_modal(Modal(num_opciones))
 
     @nextcord.slash_command(name="rae")
     async def rae(self, interaction: Interaction, palabra: str):
